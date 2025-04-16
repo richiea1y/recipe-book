0ffac113a71fdb9d9e24105b69a2a90219b8e804
richiea1y <44350561+richiea1y@users.noreply.github.com>
2025-04-16 18:00:55 +0800

    chore(clean up): clean up the project, only want to keep own custom views.

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import HomeView from '../views/HomeView.vue';
 
+// We can use createRouter function, we assign the result of this function to the router variable.
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -9,15 +10,7 @@ const router = createRouter({
       name: 'home',
       component: HomeView,
     },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
-    },
   ],
-})
+});
 
-export default router
+export default router;
