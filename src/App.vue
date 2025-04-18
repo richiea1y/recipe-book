805978db0ea82711524971ffdf27ab46c43771c0
richiea1y <44350561+richiea1y@users.noreply.github.com>
2025-04-18 14:19:14 +0800

    feat(Programmatic navigation): direct someone to a specific page without user clicking any of the links explicitly
    - "Simulate the form submission": Typically after you create a resource, for example a recipe, you just might want to redirect the user to this newly created recipe page.
    
    - "Add a link to this not ready yet form inside the App component": add another entry to this list of menu items and this leads to the route which name add the route name is “add-recipe” also add same label.
    
    - "Go back to the form(AddRecipeView) simulate adding a form element"
    
    - "When the form is submitted, we'd like to do the programmatic navigation.": add a function that can be called "addRecipe", that's the place where we're gonna add this programmatic navigation
    
    - Make sure it's run when the form is submitted.
    
    - To get access to the router, we call the use router function: It has the .push() method which lets us navigate to a link, and this also will be kept in the history. So someone can use the forward and back button in the browser.
    
    - router.push() can accept a couple of parameters. I'm going to link to this page where you can see all the different examples of using either "path" or "name". Keep in mind if you are using the "path", you can't specify parameters. Parameters can only be specified if you use the route name.
    
    - We can just direct someone to the recipe, and the route name is basically "recipe" the parameter name is "ID", the only param here is "ID" and let's simulate something. Let it be "1" always because this doesn't really matter. Our app is not ready yet.
    
    So this addRecipe should be called as soon as the form is submitted. That obviously we are simulating that something is being saved here. Nothing really happens yet. So when i  hit this form and I'm redirected to his URL which is "recipe 1"

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,9 @@ import { RouterView } from 'vue-router';
         <li>
           <RouterLink :to="{ name: 'favorites' }">Favorites</RouterLink>
         </li>
+        <li>
+          <RouterLink :to="{ name: 'add-recipe' }">Add Recipe</RouterLink>
+        </li>
       </ul>
     </nav>
   </header>
